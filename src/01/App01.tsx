6a8e71d716adbf7070de112e655949b75e88a214
Moriac01 <moriackpangam@gmail.com>
2025-01-31 21:07:22 +0100

    first commit

---
+++ src/01/App01.tsx
@@ -0,0 +1,13 @@
+import log from './web.jpg'
+function App01 (){
+    return (
+        <div className="t01">
+            <div>
+                <img src={log} alt="" width={300} className='img'/>
+            </div>
+            Mon premier composant React
+        </div>
+    )
+}
+
+export default App01
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+
+import { createRoot } from 'react-dom/client'
+import App01 from './01/App01.tsx'
+import './01/style.css'
+import './01/web.jpg'
+
+createRoot(document.getElementById('root')!).render(
+
+    <App01 />
+)
